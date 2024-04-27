7342f7d62eaf2a7a5e80566625b394405fb18cb1
sarahmae <168287344+SarahMaeLlave@users.noreply.github.com>
2024-04-27 23:13:29 +0800

    Update AboutBox1.Designer.cs

--- AboutBox1.Designer.cs
+++ AboutBox1.Designer.cs
@@ -130,7 +130,7 @@
             this.labelCompanyName.Name = "labelCompanyName";
             this.labelCompanyName.Size = new System.Drawing.Size(361, 21);
             this.labelCompanyName.TabIndex = 22;
-            this.labelCompanyName.Text = "ESC-Key Technologies\r\n";
+            this.labelCompanyName.Text = "ESC-Key Technologies 2024\r\n";
             this.labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // textBoxDescription
